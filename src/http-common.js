24e7abf9511f5a0580d0b6a7ab40cf49bf00d32c
Stasy <t.hsuanhsieh@gmail.com>
2023-05-13 01:39:08 +0800

    change baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -7,8 +7,7 @@ import axios from "axios";
 
 export default axios.create({
 
-     baseURL:"http://localhost:8080/api",
-     //baseURL:"https://reactnotesbackend-stasy.herokuapp.com/api",
+     baseURL:"http://159.223.52.208:8080/api",
      
      headers:{
       "Content-type":"application/json"
