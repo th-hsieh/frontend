be1e3388bae0b511f1769fa8be79389902fcb5db
Stasy <t.hsuanhsieh@gmail.com>
2023-05-15 13:32:53 +0800

    add the import Moment from react-moment to show the latest-updated image

--- src/components/NoteDetails.js
+++ src/components/NoteDetails.js
@@ -2,6 +2,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import NotesService from "../services/NotesService";
+import Moment from 'react-moment';
+
 
 const NoteDetails= () => {
     const {id} = useParams();
@@ -33,22 +35,24 @@ const NoteDetails= () => {
        history(`/notes/edit/${id}`);
     }
         
-
-
     return ( 
         <div className="note-details main-content">
-            <article>
-                <h5 className="text-capitalize primary-color">{currentNote.title}</h5>
-                <div className="mb-3 font-italic metadata">
-                    <span>{currentNote.updatedAt}</span>
-                    <span className="text-capitalize">, {currentNote.category}</span>
-                </div>
-                <div className="mb-3">{currentNote.body}</div>
-            </article>
-            <button onClick={handleEdit}>Edit</button>
-            <button onClick={handleDelete} className="ms-3">Delete</button>
+           {
+                currentNote &&  <div>
+                                    <article>
+                                        <h5 className="text-capitalize primary-color">{currentNote.title}</h5>
+                                        <div className="mb-3 font-italic metadata">
+                                            <Moment fromNow>{currentNote.updatedAt}</Moment>
+                                            <span className="text-capitalize">, {currentNote.category}</span>
+                                        </div>
+                                        <div className="mb-3">{currentNote.body}</div>
+                                    </article>
+                                    <button onClick={handleEdit}>Edit</button>
+                                    <button onClick={handleDelete} className="ms-3">Delete</button>
+                                </div>
+        }
         </div>
     );
 }
- 
+
 export default NoteDetails;
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import NotesService from "../services/NotesService";
 import {Link} from "react-router-dom";
+import Moment from "react-moment"
 
 const NotesList = () => {
 
@@ -25,11 +26,11 @@ const NotesList = () => {
             <h4>List of Notes</h4>
             <div className="notes-list mt-4">
                 {
-                    notes.length>0 ? notes.map(note => (
+                    notes.length >0 ? notes.map(note => (
                         <div key={note.id} className="notes-preview mt-3">
                             <Link to={`/notes/${note.id}`} className="text-decoration-none">
                                 <h5 className="primary-color text-capitalize">{note.title}</h5>
-                                <h5>{note.body}</h5>
+                                <Moment fromNow className="text-italic">{note.updatedAt}</Moment>
                             </Link>
                         </div>
                     )):<div>No notes available</div>
